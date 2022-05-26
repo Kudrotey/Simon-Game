cd9df62b0c6e06e3557e5db0208bca982bad6cf0
Kudrotey <kudrotey.khuda@madetech.com>
2022-05-26 19:29:01 +0100

    Simon Game, using vanilla JS

---
+++ game.js
@@ -0,0 +1,77 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var gameStarted = false;
+var level = 0;
+
+$(document).keydown(function() {
+    if(!gameStarted) {
+        $("#level-title").text(`Level ${level}`);
+        nextSequence();
+        gameStarted = true;
+    }
+})
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length-1);
+})
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if(gamePattern.length === userClickedPattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        var gameOverSound = new Audio(`sounds/wrong.mp3`);
+        gameOverSound.play();
+
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text(`Game Over, Press Any Key to Restart`);
+        startOver();
+    }
+}
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text(`Level ${level}`);
+    
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+    animatePress(randomChosenColour);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
+}
+
+function playSound(name) {
+    var audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $(`.${currentColour}`).addClass("pressed");
+    setTimeout(function() {
+        $(`.${currentColour}`).removeClass("pressed");
+    }, 100);
+}
